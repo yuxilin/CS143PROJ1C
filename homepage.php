5f4e18b836cdc17f6618ab278b90db2f4189e4c2
yuxi <ylin@axs.com>
2014-02-02 22:05:27 -0800

    homepage styles

---
+++ homepage.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+	<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+    <title>The Movie Database</title>
+</head>
+
+<body>
+    <h1 id="header">better than imdb</h1>
+	<div id="divider"></div>
+	<div id="nav-sidebar">MAP
+            <ul id="nav-options">
+                <li><a href="">SEARCH</a></li>
+                <li><a href="">PEOPLE DIRECTORY</a></li>
+                <li><a href="">MOVIE DIRECTORY</a></li>
+                <li><a href="">ACTORS+MOVIES</a></li>
+                <li><a href="">DIRECTORS+MOVIES</a></li>
+                <li><a href="">ADD SOMEONE IMPORTANT</a></li>
+                <li><a href="">ADD MOVIE</a></li>
+                <li><a href="">IN YOUR OPINION...</a></li>
+
+
+            </ul>
+        </div>
+	<div id="maincontent">MAIN CONTENT BLAHBLAH</div>
+
+</body>
+
+
+</html>
