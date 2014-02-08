cc931059cd3e6e8c5153438f5451db8dbdce336b
Arun Joseph <ajoseps@gmail.com>
2014-02-07 17:17:25 -0800

    got searchable movie database

--- search.php
+++ search.php
@@ -21,8 +21,49 @@
                 <li><a href="add_movie.php">ADD MOVIE</a></li>
                 <li><a href="comments.php">IN YOUR OPINION...</a></li>
             </ul>
+            <form action="search.php" method="post">
+                <input class="styled-tbox" type="text" name="search_value" value="Search" maxlength="4" type="number" onfocus="if(this.value == 'Search') { this.value = ''; }"> <br />
+                <input type="submit" value="Search">
+                <?php
+                    $search_input = $_POST['search_value'];
+                    $search_input = trim($search_input);
+                    $search_arr = explode(' ', $search_input);
+                    
+                    for($i = 0; $i < count($search_arr); $i++)
+                    {
+                        $search_arr[$i] = trim($search_arr[$i]);
+                    }
+
+                    $db_connection = mysql_connect('localhost', 'cs143', '');
+                    mysql_select_db('CS143', $db_connection);
+
+                    $query_actor = mysqL_query("SELECT * FROM Actor WHERE (SELECT CONCAT(first, ' ', last) AS name FROM Actor) LIKE '%$search_input%' ;", $db_connection);
+                    $query_movie = mysql_query("SELECT * FROM Movie WHERE title LIKE '%$search_input%' ;", $db_connection);
+                ?>
+            </form>
         </div>
     <div id="maincontent">
+        <?php
+            var_dump($query_actor);
+            echo "<br />";
+            echo "You Searched ['$search_input'] From Actor Database: ";
+            while ($row = mysql_fetch_array($query_actor)) {
+                echo "<div>";
+                    echo "Actor: " . $row['first'] . ' '. $row['last'] . ' '. '(' . $row['dob'] . ')';
+                echo "</div>";
+            }
+            echo "<br />";
+
+            echo "You Searched ['$search_input'] From Movie Database: ";
+            while ($row = mysql_fetch_array($query_movie)) {
+                echo "<div>";
+                    echo "Movie: " . $row['title'] . ' ' . '(' . $row['year'] . ')';
+                echo "</div>";
+            }
+            echo "<br />";
+
+            mysql_close($db_connection);
+        ?>
     </div>
 </body>
 
